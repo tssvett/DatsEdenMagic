5178cd4cbe5046408a7f532a0c2be283977c0bd3
tssvett <tssvett@mail.ru>
2024-10-11 22:49:31 +0400

    feat: added movement to nearest bounty

--- src/main/java/dev/team/game/controller/movement/MoveImpl.java
+++ src/main/java/dev/team/game/controller/movement/MoveImpl.java
@@ -1,8 +1,13 @@
 package dev.team.game.controller.movement;
 
+import dev.team.models.Bounty;
 import dev.team.models.TransportResponse;
 import dev.team.models.Vector2D;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
+@Slf4j
 public class MoveImpl implements Move {
     private static final double MAX_ACCELERATION = 10.0; // Максимальное ускорение (например, 10 м/с²)
 
@@ -35,5 +40,31 @@ public class MoveImpl implements Move {
 
         // Если длина равна нулю, возвращаем нулевое ускорение
         return new Vector2D(0.0, 0.0);
+    }
+
+    public Vector2D getAccelerationToNearestBounty(TransportResponse myShip, List<Bounty> bountyList) {
+        if (bountyList == null || bountyList.isEmpty()) {
+            log.info("Корабль {} не может плыть к монеткам потому что их нет", myShip.getId());
+            return null;
+        }
+
+        //Поиск ближайшей монетки для данного корабля
+
+        Bounty nearestBounty = null;
+        double minDistance = Double.MAX_VALUE;
+        for (Bounty bounty : bountyList) {
+            double distance = calculateDistance(myShip.getX(), myShip.getY(), bounty.getX(), bounty.getY());
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearestBounty = bounty;
+            }
+        }
+        return getAccelerationToPoint(myShip, new Vector2D(nearestBounty.getX().doubleValue(), nearestBounty.getY().doubleValue()));
+
+
+    }
+
+    private double calculateDistance(int x1, int y1, int x2, int y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 }
