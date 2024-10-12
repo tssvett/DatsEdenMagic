032a7a42c3aa9f25dd82a0e193092e18612e65a6
ilya_groza <ilya.groza13@gmail.com>
2024-10-12 11:16:30 +0400

    choose BountyList

---
+++ src/main/java/dev/team/game/controller/bountychoose/BountyChoose.java
@@ -0,0 +1,8 @@
+package dev.team.game.controller.bountychoose;
+
+import dev.team.game.workvector.Coordinate;
+import dev.team.models.TransportResponse;
+
+public interface BountyChoose {
+    public Coordinate bountyChoose(TransportResponse transportResponse);
+}
---
+++ src/main/java/dev/team/game/controller/bountychoose/BountyChooseImp.java
@@ -0,0 +1,12 @@
+package dev.team.game.controller.bountychoose;
+
+import dev.team.game.workvector.Coordinate;
+import dev.team.models.TransportResponse;
+
+public class BountyChooseImp implements BountyChoose{
+
+    @Override
+    public Coordinate bountyChoose(TransportResponse transportResponse) {
+        return null;
+    }
+}
